6525423e81fd61076b4cd84340f7a8b4997de06e
Alex Mathew <alexmathew003@gmail.com>
2020-07-25 11:12:59 +0530

    Create draft model

--- draftnik/drafter/models.py
+++ draftnik/drafter/models.py
@@ -1,3 +1,22 @@
+import uuid
+from django.conf import settings
+
+from django.contrib.postgres.fields import JSONField
 from django.db import models
 
-# Create your models here.
+
+def get_random_draft_name():
+    return f"Draft {uuid.uuid4().hex[:9].upper()}"
+
+
+class Draft(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name="drafts", on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=100, default=get_random_draft_name)
+    gameweek = models.IntegerField(blank=False, null=False)
+    entries = JSONField(default=list)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"#{self.gameweek}- {self.name} ({self.user})"
