e489836762f0b7c60a359300f18c6e39800cd9d1
Alex Mathew <alexmathew003@gmail.com>
2020-07-26 10:25:49 +0530

    Store team data from static in redis

--- draftnik/draftnik/keys.py
+++ draftnik/draftnik/keys.py
@@ -1,3 +1,7 @@
 from django.utils.text import slugify
 
-PLAYER_ID_KEY = lambda name, team_code: f"{team_code}_{slugify(name)}"
+PLAYER_ID_PREFIX = "PLAYER_ID_"
+PLAYER_ID_KEY = lambda name, team_code: f"{PLAYER_ID_PREFIX}{team_code}_{slugify(name)}"
+
+TEAM_ID_PREFIX = "TEAM_ID_"
+TEAM_ID_KEY = lambda team_id: f"{TEAM_ID_PREFIX}{team_id}"
--- draftnik/jobs/fpl_static.py
+++ draftnik/jobs/fpl_static.py
@@ -3,13 +3,11 @@ from collections import defaultdict
 
 import requests
 
-from draftnik.keys import PLAYER_ID_KEY
+from draftnik.keys import PLAYER_ID_KEY, TEAM_ID_KEY
 from helpers.instances import redis
 
 
 def store_players(r):
-    players_by_team_and_name = defaultdict(dict)
-
     players = iter(r.json()["elements"])
     for player in players:
         player_id = player.get("id")
@@ -19,8 +17,30 @@ def store_players(r):
         redis.set(PLAYER_ID_KEY(web_name, team_code), player_id)
 
 
-def fetch_static_data():
+def store_teams(r):
+    teams = iter(r.json()["teams"])
+    for team in teams:
+        team_id = team.get("id")
+        team_code = team.get("code")
+        name = team.get("name")
+        short_name = team.get("short_name")
+
+        data = {
+            "id": team_id,
+            "code": team_code,
+            "name": name,
+            "short_name": short_name,
+        }
+
+        redis.set(TEAM_ID_KEY(team_id), json.dumps(data))
+
+
+def fetch_static_data(players=True, teams=False):
     URL = "https://fantasy.premierleague.com/api/bootstrap-static/"
     r = requests.get(URL)
 
-    store_players(r)
+    if players:
+        store_players(r)
+
+    if teams:
+        store_teams(r)
