66b0c77ff8f165ab9972890acf047d3d185197e1
Alex Mathew <alexmathew003@gmail.com>
2020-07-26 16:01:56 +0530

    Make changes in the draft model + admin

--- draftnik/drafter/admin.py
+++ draftnik/drafter/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import Draft
 
-admin.site.register(Draft)
+
+@admin.register(Draft)
+class DraftAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "gameweek", "name", "created_at")
--- draftnik/drafter/models.py
+++ draftnik/drafter/models.py
@@ -13,10 +13,16 @@ class Draft(models.Model):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL, related_name="drafts", on_delete=models.CASCADE
     )
-    name = models.CharField(max_length=100, default=get_random_draft_name)
+    name = models.CharField(max_length=256, default=get_random_draft_name)
     gameweek = models.IntegerField(blank=False, null=False)
     entries = JSONField(default=list)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["user"], name="user_index"),
+        ]
+        ordering = ["-created_at"]
 
     def __str__(self):
         return f"#{self.gameweek}- {self.name} ({self.user})"
