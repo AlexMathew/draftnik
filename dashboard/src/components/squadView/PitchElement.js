5e55f1c3a852fd3e5bc65ca0889f9f30cd86fc88
Alex Mathew <alexmathew003@gmail.com>
2020-08-02 01:14:32 +0530

    Display jersey as part of element

--- dashboard/src/components/squadView/PitchElement.js
+++ dashboard/src/components/squadView/PitchElement.js
@@ -2,14 +2,42 @@ import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+import { connect } from "react-redux";
 
-const styles = () => ({});
+const styles = (theme) => ({
+  jerseyContainer: {
+    textAlign: "center",
+  },
+  jersey: {
+    [theme.breakpoints.up("lg")]: {
+      height: "8vh",
+    },
+    height: "5vh",
+  },
+});
 
 class PitchElement extends React.Component {
   render() {
-    const { classes, element } = this.props;
+    const { classes, element, teams } = this.props;
+    const team = teams[element.team];
 
-    return <Typography>{element.web_name}</Typography>;
+    return (
+      <Grid direction="column" justify="center" alignItems="center">
+        <Grid item xs className={classes.jerseyContainer}>
+          <img
+            className={classes.jersey}
+            src={`jerseys/${team.id}.png`}
+            alt={team.name}
+          />
+        </Grid>
+        <Grid item xs>
+          <Typography>
+            {element.web_name} ({team.short_name})
+          </Typography>
+        </Grid>
+      </Grid>
+    );
   }
 }
 
@@ -17,4 +45,11 @@ PitchElement.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(PitchElement);
+const mapStateToProps = (state) => {
+  return {
+    teams: state.teams,
+  };
+};
+
+const wrappedPitchElement = connect(mapStateToProps)(PitchElement);
+export default withStyles(styles)(wrappedPitchElement);
