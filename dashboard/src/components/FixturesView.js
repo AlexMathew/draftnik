b3e005accb324736231c8b7f69b537a82c92b893
Alex Mathew <alexmathew003@gmail.com>
2020-08-01 14:56:18 +0530

    Hide fixtures drawer on mobile

--- dashboard/src/components/FixturesView.js
+++ dashboard/src/components/FixturesView.js
@@ -1,8 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
+import Hidden from "@material-ui/core/Hidden";
 import Drawer from "@material-ui/core/Drawer";
 import Divider from "@material-ui/core/Divider";
+import Typography from "@material-ui/core/Typography";
 import { connect } from "react-redux";
 import {} from "../actions";
 
@@ -10,31 +12,51 @@ const drawerWidth = 350;
 
 const styles = (theme) => ({
   drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
+    [theme.breakpoints.up("sm")]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
   },
   drawerPaper: {
     width: drawerWidth,
   },
   toolbar: theme.mixins.toolbar,
+  content: {
+    display: "flex",
+    alignItems: "center",
+    alignSelf: "center",
+  },
 });
 
 class FixturesView extends React.Component {
   render() {
     const { classes } = this.props;
+    const fixtures = (
+      <div className={classes.content}>
+        <Divider />
+        <Typography variant="h5">FIXTURES</Typography>
+      </div>
+    );
 
     return (
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-        anchor="right"
-      >
-        <div className={classes.toolbar} />
-        <Divider />
-      </Drawer>
+      <nav className={classes.drawer}>
+        <Hidden smUp implementation="js">
+          {fixtures}
+        </Hidden>
+        <Hidden xsDown implementation="js">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            variant="permanent"
+            open
+            anchor="right"
+          >
+            <div className={classes.toolbar} />
+            {fixtures}
+          </Drawer>
+        </Hidden>
+      </nav>
     );
   }
 }
