c4e970484afb08f2d91c7391981f019b68e39d8b
Alex Mathew <alexmathew003@gmail.com>
2020-08-02 21:49:53 +0530

    Display next 1 fixture on mobile and next 3 otherwise

--- dashboard/src/components/squadView/PitchElementFixtures.js
+++ dashboard/src/components/squadView/PitchElementFixtures.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
+import Hidden from "@material-ui/core/Hidden";
 import Grid from "@material-ui/core/Grid";
 import { connect } from "react-redux";
 import _ from "lodash";
@@ -21,38 +22,52 @@ const styles = (theme) => ({
 });
 
 class PitchElementFixtures extends React.Component {
-  render() {
-    const { classes, element, teams, teamFixtures } = this.props;
+  getNextFixtures = (count) => {
     const selectedGameweek = parseInt(this.props.selectedGameweek);
     const fixtures = _.pick(
-      teamFixtures[element.team],
-      _.range(selectedGameweek, selectedGameweek + 3)
+      this.props.teamFixtures[this.props.element.team],
+      _.range(selectedGameweek, selectedGameweek + count)
     );
+
+    return fixtures;
+  };
+
+  getFixturesGrid = (count) => {
+    const { classes, element, teams } = this.props;
+
+    return _.values(this.getNextFixtures(count)).map((gwFixtures, index) => (
+      <Grid
+        item
+        xs
+        className={classes.gwFixture}
+        key={`${element.id}_${index}`}
+      >
+        <Grid container direction="column">
+          {gwFixtures.map((fixture) => (
+            <Grid
+              item
+              xs
+              key={`${element.id}_${fixture.gw}-${fixture.opponent}${fixture.location}`}
+            >
+              {`${teams[fixture.opponent].short_name} (${fixture.location})`}
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+    ));
+  };
+
+  render() {
+    const { classes } = this.props;
 
     return (
       <Grid container direction="row" className={classes.fixtures}>
-        {_.values(fixtures).map((gwFixtures, index) => (
-          <Grid
-            item
-            xs
-            className={classes.gwFixture}
-            key={`${element.id}_${index}`}
-          >
-            <Grid container direction="column">
-              {gwFixtures.map((fixture) => (
-                <Grid
-                  item
-                  xs
-                  key={`${element.id}_${fixture.gw}-${fixture.opponent}`}
-                >
-                  {`${teams[fixture.opponent].short_name} (${
-                    fixture.location
-                  })`}
-                </Grid>
-              ))}
-            </Grid>
-          </Grid>
-        ))}
+        <Hidden smUp implementation="js">
+          {this.getFixturesGrid(1)}
+        </Hidden>
+        <Hidden xsDown implementation="js">
+          {this.getFixturesGrid(3)}
+        </Hidden>
       </Grid>
     );
   }
