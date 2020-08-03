21db48467f46c1b34d52f40b39fc741111a3680b
Alex Mathew <alexmathew003@gmail.com>
2020-08-03 19:46:41 +0530

    Make changes to app bar look

--- dashboard/src/components/Header.js
+++ dashboard/src/components/Header.js
@@ -4,9 +4,9 @@ import { withStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import { connect } from "react-redux";
 import { switchMobile } from "../actions";
 import { AUTH_TOKEN_FIELD } from "../constants";
@@ -15,6 +15,8 @@ import history from "../history";
 const styles = (theme) => ({
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
+    backgroundColor: "white",
+    color: "black",
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -51,14 +53,14 @@ class Header extends React.Component {
           <Typography variant="h6" color="inherit" className={classes.grow}>
             Draftnik
           </Typography>
-          <Button
+          <IconButton
             color="inherit"
             onClick={() => {
               logout();
             }}
           >
-            Logout
-          </Button>
+            <ExitToAppIcon fontSize="large" />
+          </IconButton>
         </Toolbar>
       </AppBar>
     );
