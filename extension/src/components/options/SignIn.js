c50818aa3956418eba10678884cc0d62cf94af00
Alex Mathew <alexmathew003@gmail.com>
2020-08-08 15:45:51 +0530

    Create a base class for the auth components
    I don't know if this is best practice in the JS world. But it's much better than writing the whole component class twice.

--- extension/src/components/options/SignIn.js
+++ extension/src/components/options/SignIn.js
@@ -1,54 +1,9 @@
-import React from "react";
 import PropTypes from "prop-types";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
 import withStyles from "@material-ui/core/styles/withStyles";
-import Container from "@material-ui/core/Container";
-import { red } from "@material-ui/core/colors";
-import { Link as RouterLink } from "react-router-dom";
-import draftnik from "../../api/draftnik";
 import { AUTH_TOKEN_FIELD } from "../../constants";
+import { AuthFlow, styles } from "./AuthFlow";
 
-const styles = (theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    width: theme.spacing(7),
-    height: theme.spacing(7),
-  },
-  form: {
-    width: "100%",
-    marginTop: theme.spacing(3),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-  error: {
-    fontSize: theme.spacing(2),
-    color: red[500],
-  },
-});
-
-class SignIn extends React.Component {
-  state = {
-    username: "",
-    password: "",
-    error: {
-      general: "",
-      username: "",
-      password: "",
-    },
-  };
-
+class SignIn extends AuthFlow {
   componentDidMount() {
     const state = this.props.location.state;
     if (state) {
@@ -60,115 +15,20 @@ class SignIn extends React.Component {
     }
   }
 
-  finishLogin = ({ auth_token, username }) => {
+  headerText = "Sign In";
+  submitUrl = "/auth/token/login/";
+  buttonText = "Sign In";
+  footerLink = "/signup";
+  footerText = "Don't have an account? Sign Up";
+
+  onSubmitSuccess = (input, response) => {
+    const { username } = input;
+    const { auth_token } = response.data;
     chrome.storage.local.set({
       [AUTH_TOKEN_FIELD]: { auth_token, username },
     });
     this.props.history.push("/");
   };
-
-  login = (event) => {
-    event.preventDefault();
-    const { username, password } = this.state;
-    const fields = ["username", "password"];
-
-    draftnik
-      .post("/auth/token/login/", {
-        username,
-        password,
-      })
-      .then((response) => {
-        const { auth_token } = response.data;
-        this.finishLogin({ auth_token, username });
-      })
-      .catch((err) => {
-        if (err.response) {
-          const error = {};
-          const data = err.response.data;
-          error.general = data.non_field_errors;
-          fields.forEach((field) => {
-            error[[field]] = (data[[field]] || []).join(" ");
-          });
-          this.setState({ error });
-        }
-      });
-  };
-
-  render() {
-    const { classes } = this.props;
-    const { error } = this.state;
-
-    return (
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <div className={classes.paper}>
-          <Avatar src="/icons/logo128.png" className={classes.avatar} />
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Typography component="h4" variant="h6" className={classes.error}>
-            {error.general || ""}
-          </Typography>
-          <form className={classes.form} noValidate onSubmit={this.login}>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  variant="outlined"
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="username"
-                  label="Username"
-                  name="username"
-                  autoComplete="username"
-                  autoFocus
-                  onChange={(e) => {
-                    this.setState({ username: e.target.value });
-                  }}
-                  error={error.username !== undefined && error.username !== ""}
-                  helperText={error.username}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  variant="outlined"
-                  margin="normal"
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="current-password"
-                  onChange={(e) => {
-                    this.setState({ password: e.target.value });
-                  }}
-                  error={error.password !== undefined && error.password !== ""}
-                  helperText={error.password}
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Sign In
-            </Button>
-            <Grid container justify="flex-end">
-              <Grid item>
-                <RouterLink to="/signup">
-                  Don't have an account? Sign Up
-                </RouterLink>
-              </Grid>
-            </Grid>
-          </form>
-        </div>
-      </Container>
-    );
-  }
 }
 
 SignIn.propTypes = {
--- extension/src/components/options/SignUp.js
+++ extension/src/components/options/SignUp.js
@@ -1,157 +1,20 @@
-import React from "react";
 import PropTypes from "prop-types";
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
 import withStyles from "@material-ui/core/styles/withStyles";
-import Container from "@material-ui/core/Container";
-import { red } from "@material-ui/core/colors";
-import { Link as RouterLink } from "react-router-dom";
-import draftnik from "../../api/draftnik";
+import { AuthFlow, styles } from "./AuthFlow";
 
-const styles = (theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    width: theme.spacing(7),
-    height: theme.spacing(7),
-  },
-  form: {
-    width: "100%",
-    marginTop: theme.spacing(3),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-  error: {
-    fontSize: theme.spacing(2),
-    color: red[500],
-  },
-});
+class SignUp extends AuthFlow {
+  headerText = "Sign Up";
+  submitUrl = "/auth/users/";
+  buttonText = "Sign Up";
+  footerLink = "/signin";
+  footerText = "Already have an account? Sign in";
 
-class SignUp extends React.Component {
-  state = {
-    username: "",
-    password: "",
-    error: {
-      general: "",
-      username: "",
-      password: "",
-    },
+  onSubmitSuccess = () => {
+    this.props.history.push({
+      pathname: "/signin",
+      state: { success: true },
+    });
   };
-
-  signup = (event) => {
-    event.preventDefault();
-    const { username, password } = this.state;
-    const fields = ["username", "password"];
-
-    draftnik
-      .post("/auth/users/", {
-        username,
-        password,
-      })
-      .then(() => {
-        this.props.history.push({
-          pathname: "/signin",
-          state: { success: true },
-        });
-      })
-      .catch((err) => {
-        if (err.response) {
-          const error = {};
-          const data = err.response.data;
-          error.general = data.non_field_errors;
-          fields.forEach((field) => {
-            error[[field]] = (data[[field]] || []).join(" ");
-          });
-          this.setState({ error });
-        }
-      });
-  };
-
-  render() {
-    const { classes } = this.props;
-    const { error } = this.state;
-
-    return (
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <div className={classes.paper}>
-          <Avatar src="/icons/logo128.png" className={classes.avatar} />
-          <Typography component="h1" variant="h5">
-            Sign up
-          </Typography>
-          <Typography component="h4" variant="h6" className={classes.error}>
-            {error.general || ""}
-          </Typography>
-          <form className={classes.form} noValidate onSubmit={this.signup}>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  variant="outlined"
-                  margin="normal"
-                  required
-                  fullWidth
-                  id="username"
-                  label="Username"
-                  name="username"
-                  autoComplete="username"
-                  autoFocus
-                  onChange={(e) => {
-                    this.setState({ username: e.target.value });
-                  }}
-                  error={error.username !== undefined && error.username !== ""}
-                  helperText={error.username}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  variant="outlined"
-                  margin="normal"
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="current-password"
-                  onChange={(e) => {
-                    this.setState({ password: e.target.value });
-                  }}
-                  error={error.password !== undefined && error.password !== ""}
-                  helperText={error.password}
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              Sign Up
-            </Button>
-            <Grid container justify="flex-end">
-              <Grid item>
-                <RouterLink to="/signin">
-                  Already have an account? Sign in
-                </RouterLink>
-              </Grid>
-            </Grid>
-          </form>
-        </div>
-      </Container>
-    );
-  }
 }
 
 SignUp.propTypes = {
