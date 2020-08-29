12261ec447f0cbb92d65d679abaf66558e92fe7e
Alex Mathew <alexmathew003@gmail.com>
2020-08-29 23:21:36 +0530

    Update auth flow in extension

--- extension/src/components/options/SignIn.js
+++ extension/src/components/options/SignIn.js
@@ -1,9 +1,83 @@
+import React from "react";
 import PropTypes from "prop-types";
 import withStyles from "@material-ui/core/styles/withStyles";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { red } from "@material-ui/core/colors";
+import { Link as RouterLink } from "react-router-dom";
+import Copyright from "./Copyright";
+import draftnik from "../../api/draftnik";
 import { AUTH_TOKEN_FIELD } from "../../constants";
-import { AuthFlow, styles } from "./AuthFlow";
 
-class SignIn extends AuthFlow {
+export const styles = (theme) => ({
+  root: {
+    height: "100vh",
+  },
+  image: {
+    backgroundImage: "url(/icons/logo512.png)",
+    backgroundRepeat: "no-repeat",
+    backgroundColor:
+      theme.palette.type === "light"
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+  },
+  signin: {
+    display: "grid",
+    placeItems: "center",
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  form: {
+    width: "100%",
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    display: "grid",
+    placeItems: "center",
+  },
+  error: {
+    fontSize: theme.spacing(2),
+    color: red[500],
+  },
+  submitSpinner: {
+    color: "white",
+    width: `${theme.spacing(3)}px !important`,
+    height: `${theme.spacing(3)}px !important`,
+  },
+});
+
+class SignIn extends React.Component {
+  state = {
+    modalOpen: false,
+    modalUsername: "",
+    username: "",
+    password: "",
+    error: {
+      general: "",
+      username: "",
+      password: "",
+    },
+    submitting: false,
+  };
+
   componentDidMount() {
     chrome.storage.local.get([AUTH_TOKEN_FIELD], (result) => {
       const token = result[[AUTH_TOKEN_FIELD]];
@@ -16,17 +90,16 @@ class SignIn extends AuthFlow {
     if (state) {
       if (state.success) {
         this.setState({
-          error: { general: "You have created your new account." },
+          modalOpen: true,
+          modalUsername: state.username,
         });
       }
     }
   }
 
-  headerText = "Sign In";
-  submitUrl = "/auth/token/login/";
-  buttonText = "Sign In";
-  footerLink = "/signup";
-  footerText = "Don't have an account? Sign Up";
+  handleModalClose = () => {
+    this.setState({ modalOpen: false });
+  };
 
   onSubmitSuccess = (input, response) => {
     const { username } = input;
@@ -36,6 +109,148 @@ class SignIn extends AuthFlow {
     });
     this.props.history.push("/");
   };
+
+  submit = (event) => {
+    event.preventDefault();
+    this.setState({ submitting: true });
+    const { username, password } = this.state;
+    const fields = ["username", "password"];
+
+    draftnik
+      .post("/auth/token/login/", {
+        username,
+        password,
+      })
+      .then((response) => {
+        this.onSubmitSuccess({ username }, response);
+      })
+      .catch((err) => {
+        if (err.response) {
+          const error = {};
+          const data = err.response.data;
+          error.general = data.non_field_errors;
+          fields.forEach((field) => {
+            error[[field]] = (data[[field]] || []).join(" ");
+          });
+          this.setState({ error });
+        }
+      })
+      .finally(() => {
+        this.setState({ submitting: false });
+      });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { error } = this.state;
+
+    return (
+      <Grid container component="main" className={classes.root}>
+        <CssBaseline />
+        <Dialog
+          open={this.state.modalOpen}
+          onClose={this.handleModalClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            You have successfully registered on Draftnik!
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              {`${this.state.modalUsername}, you can now sign in to the Draftnik dashboard.`}
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleModalClose} color="primary">
+              Continue
+            </Button>
+          </DialogActions>
+        </Dialog>
+        <Grid item xs={false} sm={4} md={7} className={classes.image} />
+        <Grid
+          item
+          xs={12}
+          sm={8}
+          md={5}
+          component={Paper}
+          elevation={6}
+          square
+          className={classes.signin}
+        >
+          <div className={classes.paper}>
+            <Typography component="h1" variant="h4">
+              Sign in
+            </Typography>
+            <Typography component="h4" variant="h6" className={classes.error}>
+              {error.general || ""}
+            </Typography>
+            <form className={classes.form} noValidate onSubmit={this.submit}>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="username"
+                label="Username"
+                name="username"
+                autoComplete="username"
+                autoFocus
+                onChange={(e) => {
+                  this.setState({ username: e.target.value });
+                }}
+                error={error.username !== undefined && error.username !== ""}
+                helperText={error.username}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                onChange={(e) => {
+                  this.setState({ password: e.target.value });
+                }}
+                error={error.password !== undefined && error.password !== ""}
+                helperText={error.password}
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                {this.state.submitting ? (
+                  <CircularProgress
+                    classes={{
+                      root: classes.submitSpinner,
+                    }}
+                  />
+                ) : (
+                  "Sign In"
+                )}
+              </Button>
+              <Grid container justify="flex-end">
+                <Grid item>
+                  <RouterLink to={"/signup"}>
+                    Don't have an account? Sign Up
+                  </RouterLink>
+                </Grid>
+              </Grid>
+              <Box mt={5}>
+                <Copyright />
+              </Box>
+            </form>
+          </div>
+        </Grid>
+      </Grid>
+    );
+  }
 }
 
 SignIn.propTypes = {
--- extension/src/components/options/SignUp.js
+++ extension/src/components/options/SignUp.js
@@ -1,9 +1,65 @@
+import React from "react";
 import PropTypes from "prop-types";
 import withStyles from "@material-ui/core/styles/withStyles";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { red } from "@material-ui/core/colors";
+import { Link as RouterLink } from "react-router-dom";
+import Copyright from "./Copyright";
+import draftnik from "../../api/draftnik";
 import { AUTH_TOKEN_FIELD } from "../../constants";
-import { AuthFlow, styles } from "./AuthFlow";
 
-class SignUp extends AuthFlow {
+export const styles = (theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    width: theme.spacing(7),
+    height: theme.spacing(7),
+  },
+  form: {
+    width: "100%",
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    display: "grid",
+    placeItems: "center",
+  },
+  error: {
+    fontSize: theme.spacing(2),
+    color: red[500],
+  },
+  submitSpinner: {
+    color: "white",
+    width: `${theme.spacing(3)}px !important`,
+    height: `${theme.spacing(3)}px !important`,
+  },
+});
+
+class SignUp extends React.Component {
+  state = {
+    username: "",
+    password: "",
+    error: {
+      general: "",
+      username: "",
+      password: "",
+    },
+    submitting: false,
+  };
+
   componentDidMount() {
     chrome.storage.local.get([AUTH_TOKEN_FIELD], (result) => {
       const token = result[[AUTH_TOKEN_FIELD]];
@@ -13,18 +69,129 @@ class SignUp extends AuthFlow {
     });
   }
 
-  headerText = "Sign Up";
-  submitUrl = "/auth/users/";
-  buttonText = "Sign Up";
-  footerLink = "/signin";
-  footerText = "Already have an account? Sign in";
-
-  onSubmitSuccess = () => {
+  onSubmitSuccess = (username) => {
     this.props.history.push({
       pathname: "/signin",
-      state: { success: true },
+      state: { success: true, username },
     });
   };
+
+  submit = (event) => {
+    event.preventDefault();
+    this.setState({ submitting: true });
+    const { username, password } = this.state;
+    const fields = ["username", "password"];
+
+    draftnik
+      .post("/auth/users/", {
+        username,
+        password,
+      })
+      .then((response) => {
+        this.onSubmitSuccess(username);
+      })
+      .catch((err) => {
+        if (err.response) {
+          const error = {};
+          const data = err.response.data;
+          error.general = data.non_field_errors;
+          fields.forEach((field) => {
+            error[[field]] = (data[[field]] || []).join(" ");
+          });
+          this.setState({ error });
+        }
+      })
+      .finally(() => {
+        this.setState({ submitting: false });
+      });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { error } = this.state;
+
+    return (
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar src="/icons/logo128.png" className={classes.avatar} />
+          <Typography component="h1" variant="h5">
+            Sign Up
+          </Typography>
+          <Typography component="h4" variant="h6" className={classes.error}>
+            {error.general || ""}
+          </Typography>
+          <form className={classes.form} noValidate onSubmit={this.submit}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="username"
+                  label="Username"
+                  name="username"
+                  autoComplete="username"
+                  autoFocus
+                  onChange={(e) => {
+                    this.setState({ username: e.target.value });
+                  }}
+                  error={error.username !== undefined && error.username !== ""}
+                  helperText={error.username}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                  onChange={(e) => {
+                    this.setState({ password: e.target.value });
+                  }}
+                  error={error.password !== undefined && error.password !== ""}
+                  helperText={error.password}
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              {this.state.submitting ? (
+                <CircularProgress
+                  classes={{
+                    root: classes.submitSpinner,
+                  }}
+                />
+              ) : (
+                "Sign Up"
+              )}
+            </Button>
+            <Grid container justify="flex-end">
+              <Grid item>
+                <RouterLink to={"/signin"}>
+                  Already have an account? Sign in
+                </RouterLink>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+        <Box mt={8}>
+          <Copyright />
+        </Box>
+      </Container>
+    );
+  }
 }
 
 SignUp.propTypes = {
