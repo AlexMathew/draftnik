974a037dc2a3309578bbbcd12b1f8035d4919723
Alex Mathew <alexmathew003@gmail.com>
2020-09-01 23:06:43 +0530

    Add copy icon in clone button

--- dashboard/src/components/sharedDraft/CloneDraft.js
+++ dashboard/src/components/sharedDraft/CloneDraft.js
@@ -1,5 +1,14 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
 import CtaButton from "./CtaButton";
+import CopyIcon from "../utils/CopyIcon";
+
+const styles = (theme) => ({
+  icon: {
+    marginRight: theme.spacing(1),
+  },
+});
 
 class CloneDraft extends React.Component {
   handleClick = () => {
@@ -8,8 +17,21 @@ class CloneDraft extends React.Component {
   };
 
   render() {
-    return <CtaButton handleClick={this.handleClick} content="Clone Draft" />;
+    const { classes } = this.props;
+
+    const content = (
+      <>
+        <CopyIcon className={classes.icon} />
+        Clone Draft
+      </>
+    );
+
+    return <CtaButton handleClick={this.handleClick} content={content} />;
   }
 }
 
-export default CloneDraft;
+CloneDraft.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(CloneDraft);
