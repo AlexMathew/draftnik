1eb421a707f69170772b4b83bc6cc68c2b9233b9
Alex Mathew <alexmathew003@gmail.com>
2020-09-05 19:07:35 +0530

    Open news tooltip on click in mobile

--- dashboard/src/components/squadView/ElementInfo.js
+++ dashboard/src/components/squadView/ElementInfo.js
@@ -3,6 +3,8 @@ import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import InfoIcon from "@material-ui/icons/Info";
 import Tooltip from "@material-ui/core/Tooltip";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import Hidden from "@material-ui/core/Hidden";
 import { AVAILABILITY } from "../../constants";
 
 const styles = (theme) => ({
@@ -12,8 +14,15 @@ const styles = (theme) => ({
     boxShadow: theme.shadows[1],
     fontSize: "small",
   },
+  mobileTooltip: {
+    padding: theme.spacing(1),
+    maxWidth: "none",
+  },
   info: {
     borderRadius: "50%",
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "large",
+    },
   },
   available: {
     fill: "white",
@@ -38,9 +47,21 @@ const styles = (theme) => ({
 });
 
 class ElementInfo extends React.Component {
+  state = {
+    tooltipOpen: false,
+  };
+
   DOUBTFUL_75 = "75%";
   DOUBTFUL_50 = "50%";
   DOUBTFUL_25 = "25%";
+
+  handleTooltipOpen = () => {
+    this.setState({ tooltipOpen: true });
+  };
+
+  handleTooltipClose = () => {
+    this.setState({ tooltipOpen: false });
+  };
 
   playerStatusClass = () => {
     const { element } = this.props;
@@ -64,20 +85,48 @@ class ElementInfo extends React.Component {
     if (element.status === AVAILABILITY.AVAILABLE) return null;
 
     return (
-      <Tooltip
-        classes={{ tooltipArrow: classes.tooltip }}
-        title={element.news}
-        arrow
-        interactive
-        placement="top-start"
-      >
-        <InfoIcon
-          className={classes.info}
-          classes={{
-            root: classes[this.playerStatusClass()],
-          }}
-        />
-      </Tooltip>
+      <>
+        <Hidden smDown implementation="js">
+          <Tooltip
+            classes={{ tooltipArrow: classes.tooltip }}
+            title={element.news}
+            arrow
+            interactive
+            placement="top-start"
+          >
+            <InfoIcon
+              className={classes.info}
+              classes={{
+                root: classes[this.playerStatusClass()],
+              }}
+            />
+          </Tooltip>
+        </Hidden>
+        <Hidden smUp implementation="js">
+          <ClickAwayListener onClickAway={this.handleTooltipClose}>
+            <Tooltip
+              onClose={this.handleTooltipClose}
+              open={this.state.tooltipOpen}
+              classes={{
+                tooltip: classes.mobileTooltip,
+                tooltipArrow: classes.tooltip,
+              }}
+              title={element.news}
+              arrow
+              interactive
+              placement="top-start"
+            >
+              <InfoIcon
+                className={classes.info}
+                classes={{
+                  root: classes[this.playerStatusClass()],
+                }}
+                onClick={this.handleTooltipOpen}
+              />
+            </Tooltip>
+          </ClickAwayListener>
+        </Hidden>
+      </>
     );
   }
 }
