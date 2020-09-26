d990dc53a283aa96deff9a772e62e33841e6e575
Alex Mathew <alexmathew003@gmail.com>
2020-09-26 11:39:33 +0530

    Add team name in fixture

--- dashboard/src/components/fixturesView/Fixture.js
+++ dashboard/src/components/fixturesView/Fixture.js
@@ -2,16 +2,29 @@ import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 import { connect } from "react-redux";
 import { getTimeString } from "../../utils/datetime";
 
 const styles = (theme) => ({
   badge: {
+    [theme.breakpoints.down("md")]: {
+      height: "3vh",
+    },
     height: "4vh",
+    marginRight: theme.spacing(0.5),
+    marginLeft: theme.spacing(0.5),
+  },
+  teamName: {
+    [theme.breakpoints.only("md")]: {
+      fontSize: "x-small",
+    },
+    fontSize: "small",
+    fontWeight: "bold",
   },
   kickoff: {
     [theme.breakpoints.down("md")]: {
-      fontSize: "small",
+      fontSize: "x-small",
     },
     maxWidth: "8vh",
     fontWeight: "bold",
@@ -40,13 +53,39 @@ class Fixture extends React.Component {
     return (
       <Grid container direction="row" justify="center" alignItems="center">
         <Grid item xs>
-          <img className={classes.badge} src={home.badge} alt={home.name}></img>
+          <Grid container direction="row" justify="center" alignItems="center">
+            <Grid item xs>
+              <Typography className={classes.teamName}>
+                {home.shortName}
+              </Typography>
+            </Grid>
+            <Grid item xs>
+              <img
+                className={classes.badge}
+                src={home.badge}
+                alt={home.name}
+              ></img>
+            </Grid>
+          </Grid>
         </Grid>
         <Grid item xs className={classes.kickoff}>
           {getTimeString(fixture.kickoff_time)}
         </Grid>
         <Grid item xs>
-          <img className={classes.badge} src={away.badge} alt={away.name}></img>
+          <Grid container direction="row" justify="center" alignItems="center">
+            <Grid item xs>
+              <img
+                className={classes.badge}
+                src={away.badge}
+                alt={away.name}
+              ></img>
+            </Grid>
+            <Grid item xs>
+              <Typography className={classes.teamName}>
+                {away.shortName}
+              </Typography>
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     );
