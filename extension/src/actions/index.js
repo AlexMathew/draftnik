e445b1ea4b4abcf67cf8fd0303688a8a1b3d7f2d
Alex Mathew <alexmathew003@gmail.com>
2020-10-28 23:41:18 +0530

    Access static data from storage if available

--- extension/src/actions/index.js
+++ extension/src/actions/index.js
@@ -12,47 +12,85 @@ import {
 import draftnik from "../api/draftnik";
 import { AUTH_TOKEN_FIELD, ACTIONS, STATIC_DATA_FIELD } from "../constants";
 
-export const fetchStaticData = () => async (dispatch) => {
-  chrome.storage.local.get([AUTH_TOKEN_FIELD], async (result) => {
-    if (AUTH_TOKEN_FIELD in result) {
-      const { auth_token } = result[[AUTH_TOKEN_FIELD]];
-      if (auth_token !== undefined && auth_token !== null) {
-        try {
-          dispatch(startStaticLoading());
-          const response = await draftnik.get("/draft/static/", {
-            headers: {
-              Authorization: `Token ${auth_token}`,
-            },
-          });
-          chrome.storage.local.set({
-            [STATIC_DATA_FIELD]: { data: response.data },
-          });
+const getStaticDataFromStorage = async () => {
+  const storage = new Promise((resolve, reject) => {
+    chrome.storage.local.get([STATIC_DATA_FIELD], async (result) => {
+      if (STATIC_DATA_FIELD in result) {
+        console.log("STORAGE");
+        resolve(result[[STATIC_DATA_FIELD]]);
+      } else {
+        reject();
+      }
+    });
+  });
 
-          dispatch({ type: LOAD_TEAM_DATA, payload: response.data });
-          dispatch({ type: LOAD_PLAYER_DATA, payload: response.data });
-          dispatch({ type: LOAD_GAMEWEEK_DATA, payload: response.data });
-          dispatch({ type: LOAD_FIXTURES_DATA, payload: response.data });
-          dispatch({ type: LOAD_DRAFTS, payload: response.data });
+  return await storage;
+};
 
-          dispatch(selectDraft(null));
-          dispatch(selectGameweek(response.data.static.current_gameweek));
-          dispatch(stopStaticLoading());
-        } catch (error) {
-          console.error(error);
-          if (error.response?.status === 401) {
-            chrome.storage.local.remove([AUTH_TOKEN_FIELD]);
-            chrome.runtime.sendMessage({ action: ACTIONS.OPEN_OPTIONS });
+const getStaticDataFromAPI = async () => {
+  const draftnikApi = new Promise((resolve, reject) => {
+    console.log("API");
+    chrome.storage.local.get([AUTH_TOKEN_FIELD], async (result) => {
+      if (AUTH_TOKEN_FIELD in result) {
+        const { auth_token } = result[[AUTH_TOKEN_FIELD]];
+        if (auth_token !== undefined && auth_token !== null) {
+          try {
+            const response = await draftnik.get("/draft/static/", {
+              headers: {
+                Authorization: `Token ${auth_token}`,
+              },
+            });
+            chrome.storage.local.set({
+              [STATIC_DATA_FIELD]: { data: response.data },
+            });
+
+            resolve(response);
+          } catch (error) {
+            console.error(error);
+            if (error.response?.status === 401) {
+              reject("UNAUTHORIZED");
+            }
           }
+        } else {
+          reject("UNAUTHORIZED");
         }
       } else {
-        chrome.storage.local.remove([AUTH_TOKEN_FIELD]);
-        chrome.runtime.sendMessage({ action: ACTIONS.OPEN_OPTIONS });
+        reject("UNAUTHORIZED");
       }
-    } else {
+    });
+  });
+
+  return await draftnikApi;
+};
+
+const getStaticDataFromStorageOrAPI = async () => {
+  console.log("FETCH");
+  try {
+    return await getStaticDataFromStorage();
+  } catch (error) {
+    return await getStaticDataFromAPI();
+  }
+};
+
+export const fetchStaticData = () => async (dispatch) => {
+  try {
+    const response = await getStaticDataFromStorageOrAPI();
+    console.log(response);
+    dispatch({ type: LOAD_TEAM_DATA, payload: response.data });
+    dispatch({ type: LOAD_PLAYER_DATA, payload: response.data });
+    dispatch({ type: LOAD_GAMEWEEK_DATA, payload: response.data });
+    dispatch({ type: LOAD_FIXTURES_DATA, payload: response.data });
+    dispatch({ type: LOAD_DRAFTS, payload: response.data });
+
+    dispatch(selectDraft(null));
+    dispatch(selectGameweek(response.data.static.current_gameweek));
+    dispatch(stopStaticLoading());
+  } catch (err) {
+    if (err === "UNAUTHORIZED") {
       chrome.storage.local.remove([AUTH_TOKEN_FIELD]);
       chrome.runtime.sendMessage({ action: ACTIONS.OPEN_OPTIONS });
     }
-  });
+  }
 };
 
 export const selectDraft = (draft) => {
