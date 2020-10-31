d7b54affca92912ad34032c2e57caa87283a8d46
Alex Mathew <alexmathew003@gmail.com>
2020-10-31 15:44:14 +0530

    Add periodic static data sync

--- extension/src/background.js
+++ extension/src/background.js
@@ -1,5 +1,7 @@
 import { createStore } from "redux";
-import { wrapStore } from "webext-redux";
+import { wrapStore, applyMiddleware } from "webext-redux";
+import thunk from "redux-thunk";
+import { fetchStaticData } from "./actions";
 import reducers from "./reducers";
 import {
   AUTH_TOKEN_FIELD,
@@ -8,8 +10,13 @@ import {
   DASHBOARD_URL,
 } from "./constants";
 
-const store = createStore(reducers, {});
+const xstore = createStore(reducers, {});
+const middleware = [thunk];
+const store = applyMiddleware(xstore, ...middleware);
 wrapStore(store, { portName: STORE_PORT_NAME });
+
+const SYNC_STATIC_ALARM_NAME = "syncStaticData";
+const REFRESH_HOURS = 12;
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.local.get([AUTH_TOKEN_FIELD], (result) => {
@@ -22,6 +29,9 @@ chrome.runtime.onInstalled.addListener(() => {
       chrome.runtime.openOptionsPage();
     }
   });
+  chrome.alarms.create(SYNC_STATIC_ALARM_NAME, {
+    periodInMinutes: REFRESH_HOURS * 60,
+  });
 });
 
 chrome.runtime.onMessage.addListener((message) => {
@@ -32,6 +42,19 @@ chrome.runtime.onMessage.addListener((message) => {
       break;
     case ACTIONS.OPEN_DASHBOARD:
       chrome.tabs.create({ url: DASHBOARD_URL });
+      break;
+  }
+});
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+  switch (alarm.name) {
+    case SYNC_STATIC_ALARM_NAME:
+      console.log(SYNC_STATIC_ALARM_NAME);
+      const currentStore = store.getState();
+      const shouldRefresh = currentStore.transfers.saved;
+      if (shouldRefresh) {
+        store.dispatch(fetchStaticData(true));
+      }
       break;
   }
 });
