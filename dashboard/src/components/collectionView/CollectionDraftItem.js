dc7a2f7e325b8d405ca27c143f5499c7fec737c5
Alex Mathew <alexmathew003@gmail.com>
2021-03-29 23:40:47 +0530

    Select gameweek of draft from collection

--- dashboard/src/components/collectionView/CollectionDraftItem.js
+++ dashboard/src/components/collectionView/CollectionDraftItem.js
@@ -1,10 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
+import { withStyles, withTheme } from "@material-ui/core/styles";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import { connect } from "react-redux";
+import { selectDraft, selectGameweek, switchMobile } from "../../actions";
 
 const styles = (theme) => ({
   nested: {
@@ -13,11 +14,23 @@ const styles = (theme) => ({
 });
 
 class CollectionDraftItem extends React.Component {
+  selectDraft = () => {
+    const { draft } = this.props;
+    this.props.selectGameweek(draft.gameweek);
+    const vw = Math.max(
+      document.documentElement.clientWidth || 0,
+      window.innerWidth || 0
+    );
+    if (vw < this.props.theme.breakpoints.values.lg) {
+      this.props.switchMobile();
+    }
+  };
+
   render() {
     const { classes, draft } = this.props;
 
     return (
-      <ListItem button>
+      <ListItem button onClick={this.selectDraft}>
         <ListItemText primary={draft.name} />
         <ChevronRightIcon />
       </ListItem>
@@ -33,8 +46,9 @@ const mapStateToProps = (state) => {
   return {};
 };
 
-const wrappedCollectionDraftItem = connect(
-  mapStateToProps,
-  {}
-)(CollectionDraftItem);
-export default withStyles(styles)(wrappedCollectionDraftItem);
+const wrappedCollectionDraftItem = connect(mapStateToProps, {
+  selectDraft,
+  selectGameweek,
+  switchMobile,
+})(CollectionDraftItem);
+export default withTheme(withStyles(styles)(wrappedCollectionDraftItem));
